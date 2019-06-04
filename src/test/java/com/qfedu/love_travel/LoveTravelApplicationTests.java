de2a2829338d8e239a8635047ab34b80ae63d60b
renr <751299914@aliyun.com>
2019-06-04 20:55:35 +0800

    搭的架子，能正常运行，因为还没有建数据库，所以自己填充了老师的数据，进行数据运行，后期数据库建好，将数据再导入各个包里

---
+++ src/test/java/com/qfedu/love_travel/LoveTravelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qfedu.love_travel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoveTravelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
