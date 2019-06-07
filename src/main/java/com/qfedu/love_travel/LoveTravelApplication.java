d10eb29099d1d5f0a4a29a22022dab58850df21e
djl <yuuyoo@163.com>
2019-06-07 22:49:56 +0800

    1.修改开关配置文件冲突

--- src/main/java/com/qfedu/love_travel/LoveTravelApplication.java
+++ src/main/java/com/qfedu/love_travel/LoveTravelApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+
 @SpringBootApplication
 @MapperScan("com.qfedu.love_travel.dao")
 @EnableSwagger2
