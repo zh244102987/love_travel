fc6dd257478f4c673190c7f7741f73759999dda9
renr <zh244102987@qq.com>
2019-06-11 19:24:42 +0800

    完成酒店详情数据接口、酒店配套设施接口、酒店评论接口

--- src/main/java/com/qfedu/love_travel/controller/HotelcommentController.java
+++ src/main/java/com/qfedu/love_travel/controller/HotelcommentController.java
@@ -1,9 +1,14 @@
 package com.qfedu.love_travel.controller;
 
 
+import com.qfedu.love_travel.service.HotelcommentService;
+import com.qfedu.love_travel.vo.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -18,5 +23,13 @@ import org.springframework.stereotype.Controller;
 @Api(value = "查询酒店评论的接口", tags = "查询酒店评论的数据接口")
 public class HotelcommentController {
 
+    @Autowired(required = false)
+    private HotelcommentService hotelcommentService;
+
+    @GetMapping("/findCommentByDetailId.do")
+    @ApiOperation(value = "根据酒店详情id查询分页查询相关评论")
+    public R findAllCommentByDetailId(Integer currentPage, Integer size, Integer detailId) {
+        return R.setOK("OK", hotelcommentService.findAllCommentByDetailId(currentPage, size, detailId));
+    }
 }
 
