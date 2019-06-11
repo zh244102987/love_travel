803d1617bbf713d7d74004d75637f7fe4f678278
djl <yuuyoo@163.com>
2019-06-11 17:41:36 +0800

    注解缺少更新

--- src/main/java/com/qfedu/love_travel/controller/HotelcommentController.java
+++ src/main/java/com/qfedu/love_travel/controller/HotelcommentController.java
@@ -1,9 +1,9 @@
 package com.qfedu.love_travel.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
