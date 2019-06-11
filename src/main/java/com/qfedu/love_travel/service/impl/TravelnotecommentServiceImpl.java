585dc198f236a7da24bb2020371881c75b9c8697
djl <yuuyoo@163.com>
2019-06-11 17:35:32 +0800

    修改评论返回值问题

--- src/main/java/com/qfedu/love_travel/service/impl/TravelnotecommentServiceImpl.java
+++ src/main/java/com/qfedu/love_travel/service/impl/TravelnotecommentServiceImpl.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.qfedu.love_travel.dao.TravelnotecommentMapper;
 import com.qfedu.love_travel.entity.Travelnotecomment;
 import com.qfedu.love_travel.service.TravelnotecommentService;
+import com.qfedu.love_travel.vo.VTravelNoteComment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,8 +27,8 @@ public class TravelnotecommentServiceImpl extends ServiceImpl<TravelnotecommentM
 
     @Transactional(readOnly = true)
     @Override
-    public Page<Travelnotecomment> findAllCommentByNoteId(Integer currentPage, Integer size, Integer noteId) {
-        Page<Travelnotecomment> page = new Page<>(currentPage, size);
+    public Page<VTravelNoteComment> findAllCommentByNoteId(Integer currentPage, Integer size, Integer noteId) {
+        Page<VTravelNoteComment> page = new Page<>(currentPage, size);
         return page.setRecords(travelnotecommentMapper.selectAllCommentByNoteId(page, noteId));
     }
 }
